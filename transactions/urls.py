6d85aeca8e47fe2ab308e9720caf0c115f7530a1
Milos Zivanovic <milosz@pickwings.ch>
2024-10-30 01:47:26 +0100

    transactions list and chart refactoring

--- transactions/urls.py
+++ transactions/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.transaction_list, name='transaction_list'),
-    path('chart/', views.transaction_chart, name='transaction_chart'),
+    path('', views.transaction_overview, name='transaction_overview'),
     path('create/', views.transaction_create, name='transaction_create'),
     path('edit/<int:pk>/', views.transaction_edit, name='transaction_edit'),
     path('delete/<int:pk>/', views.transaction_delete, name='transaction_delete'),
--- transactions/views.py
+++ transactions/views.py
@@ -2,65 +2,60 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from django.db.models import Sum
 from django.core.paginator import Paginator
-from .models import Transaction, CategoryGroup
+from .models import Transaction
 from .forms import TransactionForm
 from .filters import TransactionFilter
 
 
-def transaction_list(request):
-    # Get transactions
+def transaction_overview(request):
+    # Filter transactions
+    show = request.GET.get('show', 'table').lower()
     today = timezone.now()
-    transactions = Transaction.objects.filter(
-        created_at__year=today.year,
-        created_at__month=today.month,
-        is_deleted=False
-    ).order_by('-created_at')
+    filterset = TransactionFilter(request.GET,
+                                  queryset=Transaction.objects.filter(is_deleted=False).order_by('-created_at'))
 
     # Calculate totals
-    filterset = TransactionFilter(request.GET, queryset=transactions)
-    total_income = filterset.qs.filter(transaction_type='income').aggregate(Sum('amount'))['amount__sum'] or 0
-    total_expense = filterset.qs.filter(transaction_type='expense').aggregate(Sum('amount'))['amount__sum'] or 0
+    total_income = filterset.qs.filter(transaction_type=Transaction.INCOME).aggregate(Sum('amount'))['amount__sum'] or 0
+    total_expense = filterset.qs.filter(transaction_type=Transaction.EXPENSE).aggregate(Sum('amount'))['amount__sum'] or 0
     balance = total_income - total_expense
-
-    # Paginate transactions, showing 10 items per page
-    paginator = Paginator(filterset.qs, 10)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-
-    get_params = ''.join([f'&{key}={value}' for key, value in request.GET.items() if key != 'page'])
-    months = ["Januar", "Februar", "Mart", "April", "Maj", "Jun", "Jul", "Avgust", "Septembar", "Oktobar", "Novembar",
-              "Decembar"]
-    return render(request, 'transactions/transaction_list.html', {
+    template_data = {
+        'show': show,
+        'filterset': filterset,
         'total_income': total_income,
         'total_expense': total_expense,
         'balance': balance,
         'balance_class': 'income' if balance > 0 else 'expense',
-        'month': months[today.month - 1],
-        'year': today.year,
-        'get_params': get_params,
-        'filterset': filterset,
-        'page_obj': page_obj
-    })
+        'current_date': today.strftime('%Y-%m-%d'),
+    }
 
+    # Prepare table data
+    if show == 'table':
+        page_number = request.GET.get('page', 1)
+        paginator = Paginator(filterset.qs, 10)
+        page_obj = paginator.get_page(page_number)
+        query_params = ''.join([f'&{key}={value}' for key, value in request.GET.items() if key != 'page'])
 
-def transaction_chart(request):
-    labels, data, total = [], [], 0
-    for cg in CategoryGroup.objects.all().exclude(name__icontains='plata'):
-        labels.append(cg.name)
-        cg_total = Transaction.objects.filter(
-            category__category_group=cg,
-            transaction_type=Transaction.EXPENSE,
-            is_deleted=False
-        ).aggregate(total_sum=Sum('amount'))['total_sum'] or 0
-        cg_total = float(cg_total)
-        data.append(cg_total)
-        total += cg_total
+        return render(request, 'transactions/transaction_overview.html', template_data | {
+            'page_obj': page_obj,
+            'query_params': query_params,
+        })
+    # Prepare chart data
+    elif show == 'chart':
+        grouped_data = (
+            filterset.qs
+            .filter(transaction_type=Transaction.EXPENSE)
+            .values('category__category_group__name')
+            .annotate(total_amount=Sum('amount'))
+        )
+        labels = [o['category__category_group__name'] for o in grouped_data]
+        data = [float(o['total_amount']) for o in grouped_data]
 
-    return render(request, 'transactions/transaction_chart.html', {
-        'labels': labels,
-        'data': data,
-        'total': total,
-    })
+        return render(request, 'transactions/transaction_overview.html', template_data | {
+            'labels': labels,
+            'data': data,
+        })
+    else:
+        raise NotImplemented()
 
 
 def transaction_create(request):
@@ -70,7 +65,7 @@ def transaction_create(request):
             transaction = form.save(commit=False)
             transaction.user = request.user
             transaction.save()
-            return redirect('transaction_list')
+            return redirect('transaction_overview')
     else:
         form = TransactionForm()
     return render(request, 'transactions/transaction_form.html', {'form': form})
@@ -82,7 +77,7 @@ def transaction_edit(request, pk):
         form = TransactionForm(request.POST, instance=transaction)
         if form.is_valid():
             form.save()
-            return redirect('transaction_list')
+            return redirect('transaction_overview')
     else:
         form = TransactionForm(instance=transaction)
     return render(request, 'transactions/transaction_form.html', {'form': form})
@@ -93,5 +88,5 @@ def transaction_delete(request, pk):
     if request.method == 'POST':
         transaction.is_deleted = True
         transaction.save()
-        return redirect('transaction_list')
+        return redirect('transaction_overview')
     return render(request, 'transactions/transaction_confirm_delete.html', {'transaction': transaction})
