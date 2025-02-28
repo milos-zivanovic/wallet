137d9a111e22956492f40f01d5c3d9d9124889a4
Milos Zivanovic <milosz@pickwings.ch>
2025-02-28 23:31:51 +0100

    dropping pyzbar

--- sales_receipts/views.py
+++ sales_receipts/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from PIL import Image
-from pyzbar.pyzbar import decode
 
 
 def qr_scan(request):
@@ -15,13 +14,14 @@ def qr_upload(request):
 
     try:
         image = Image.open(image_file)
+        return JsonResponse({"result": 'Citanje QR koda u fazi implementacije.'}, status=200)
 
-        decoded_objects = decode(image)
-        if decoded_objects:
-            qr_text = decoded_objects[0].data.decode("utf-8")
-            return JsonResponse({"result": qr_text}, status=200)
-        else:
-            return JsonResponse({"error": "QR kod nije pronađen na slici."}, status=400)
+        # decoded_objects = decode(image)
+        # if decoded_objects:
+        #     qr_text = decoded_objects[0].data.decode("utf-8")
+        #     return JsonResponse({"result": qr_text}, status=200)
+        # else:
+        #     return JsonResponse({"error": "QR kod nije pronađen na slici."}, status=400)
 
     except Exception as e:
         return JsonResponse({"error": f"Greška pri obradi slike: {str(e)}"}, status=500)
