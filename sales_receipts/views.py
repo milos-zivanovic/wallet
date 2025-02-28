f6a0f1409aaea751b442dcebfb98637a5bbc214c
Milos Zivanovic <milosz@pickwings.ch>
2025-02-28 11:14:17 +0100

    tmp removing pyzbar package

--- sales_receipts/views.py
+++ sales_receipts/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from pyzbar.pyzbar import decode
 from PIL import Image
 import io
 
@@ -15,11 +14,14 @@ def qr_upload(request):
     if request.method != 'POST' or not request.FILES.get('image'):
         return JsonResponse({'result': 'Greška u upload-u.'}, status=404)
 
-    image_data = request.FILES['image'].read()
-    image = Image.open(io.BytesIO(image_data))
-    qr_codes = decode(image)
-    if qr_codes:
-        result = qr_codes[0].data.decode('utf-8')
-        return JsonResponse({'result': result}, status=200)
-    else:
-        return JsonResponse({'result': 'QR kod nije pronađen.'}, status=404)
+    return JsonResponse({'result': 'Ok.'}, status=200)
+
+
+    # image_data = request.FILES['image'].read()
+    # image = Image.open(io.BytesIO(image_data))
+    # qr_codes = decode(image)
+    # if qr_codes:
+    #     result = qr_codes[0].data.decode('utf-8')
+    #     return JsonResponse({'result': result}, status=200)
+    # else:
+    #     return JsonResponse({'result': 'QR kod nije pronađen.'}, status=404)
