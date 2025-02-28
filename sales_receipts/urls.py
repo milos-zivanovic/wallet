fd17cbd5534dfbf08ac96e2c43bffb6833a20899
Milos Zivanovic <milosz@pickwings.ch>
2025-03-01 00:56:28 +0100

    dropping qr code scanner

--- sales_receipts/urls.py
+++ sales_receipts/urls.py
@@ -5,6 +5,5 @@ app_name = 'sales_receipts'
 
 
 urlpatterns = [
-    path('qr-scan/', views.qr_scan, name='qr_scan'),
-    path('qr-upload/', views.qr_upload, name='qr_upload'),
+    # path('qr-scan/', views.qr_scan, name='qr_scan'),
 ]
