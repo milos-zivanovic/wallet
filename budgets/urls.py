2d2f4ff040cb99d358ef158278ec12c240384930
Milos Zivanovic <milosz@pickwings.ch>
2025-11-29 14:16:34 +0100

    improving budgets app with edit and delete actions

--- budgets/urls.py
+++ budgets/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.budget_list, name='budget_list'),
     path('create/', views.budget_create, name='budget_create'),
+    path('edit/<int:pk>/', views.budget_edit, name='budget_edit'),
+    path('delete/<int:pk>/', views.budget_delete, name='budget_delete'),
 ]
--- budgets/views.py
+++ budgets/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.utils.timezone import now
+from django.shortcuts import get_object_or_404
 from .models import Budget
 from .forms import BudgetForm
 
@@ -13,7 +14,9 @@ def budget_list(request):
     active_budgets = sorted(active_budgets, key=lambda budget: budget.percentage_spent, reverse=True)
 
     # Get all budgets
-    budgets = Budget.objects.all().order_by('-start_date', 'category__category_group_id', 'category_id')
+    budgets = Budget.objects.exclude(
+        start_date__lte=today, end_date__gte=today
+    ).order_by('-start_date', 'category__category_group_id', 'category_id')
 
     return render(request, 'budgets/budget_list.html', {
         'active_budgets': active_budgets,
@@ -30,3 +33,23 @@ def budget_create(request):
     else:
         form = BudgetForm()
     return render(request, 'budgets/budget_form.html', {'form': form})
+
+
+def budget_edit(request, pk):
+    budget = get_object_or_404(Budget, pk=pk)
+    if request.method == 'POST':
+        form = BudgetForm(request.POST, instance=budget)
+        if form.is_valid():
+            form.save()
+            return redirect('budget_list')
+    else:
+        form = BudgetForm(instance=budget)
+    return render(request, 'budgets/budget_form.html', {'form': form})
+
+
+def budget_delete(request, pk):
+    budget = get_object_or_404(Budget, pk=pk)
+    if request.method == 'POST':
+        budget.delete()
+        return redirect('budget_list')
+    return render(request, 'budgets/budget_confirm_delete.html', {'budget': budget})
